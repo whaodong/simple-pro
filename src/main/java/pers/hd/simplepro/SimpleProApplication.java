48bda62b6da7cb4121d7d32e2f9d3062524032a6
wanghaodong <1186242931@qq.com>
2022-11-04 16:18:59 +0800

    初始化项目

---
+++ src/main/java/pers/hd/simplepro/SimpleProApplication.java
@@ -0,0 +1,13 @@
+package pers.hd.simplepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/hd/simplepro/SimpleProApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.hd.simplepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
